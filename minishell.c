4043d291bb713e7dcb18e10a06db26ada7e24662
samiya zubair <94432143+samiyazbr@users.noreply.github.com>
2022-11-26 12:43:56 +0000

    execve()

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 14:50:02 by szubair           #+#    #+#             */
-/*   Updated: 2022/11/25 16:17:52 by codespace        ###   ########.fr       */
+/*   Updated: 2022/11/26 12:42:00 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <readline/history.h>
 # include <readline/readline.h>
+#include <unistd.h>	/* needed to define getpid() */
 
 int main(int ac, char** av)
 {
@@ -23,16 +24,23 @@ int main(int ac, char** av)
   (void)ac;
   (void)av;
 
-  while (1)
-  {
-	buf = readline("minishell> ");
-	if (!buf)
-		exit(EXIT_FAILURE);
-    if (strlen(buf) > 0)
-      add_history(buf);
+//  while (1)
+//  {
+//	buf = readline("minishell> ");
+//	if (!buf)
+//		exit(EXIT_FAILURE);
+//    if (strlen(buf) > 0)
+//      add_history(buf);
 
-    printf("[%s]\n", buf);
-  }
+//    printf("[%s]\n", buf);
+//  }
+	char *args[] = {"ls","-la", "/", 0};	/* each element represents a command line argument */
+	char *env[] = { 0 };	/* leave the environment list null */
+
+	//printf("About to run /bin/ls\n");
+	execve("/bin/ls", args, env);
+	perror("execve");	/* if we get here, execve failed */
+	exit(1);
 
   return 0;
 }
